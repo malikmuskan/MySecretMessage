65ad7fc7b72b8805aeb8aa1adb33220b9d7d3c2f
malikmuskan <malikmuskan152009@gmail.com>
2022-10-18 19:29:56 -0400

    commit 2

--- app/src/main/java/com/example/mysecretmessage/MessageFragment.kt
+++ app/src/main/java/com/example/mysecretmessage/MessageFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
 class MessageFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view_new =inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view_new.findViewById<Button>(R.id.next)
+
+        nextButton.setOnClickListener {
+            view_new.findNavController().navigate(R.id.action_messageFragment_to_welcomeFragment)
+        }
+
+
+
+
+
+        return view_new
     }
 }
--- app/src/main/java/com/example/mysecretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/example/mysecretmessage/WelcomeFragment.kt
@@ -10,10 +10,7 @@ import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_welcome, container, false)
         val startButton = view.findViewById<Button>(R.id.start)
